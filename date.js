a3e034d1398a42e90ddffb6005c7d963dfdc9145
Gustavo Ferreira <>
2021-06-08 16:08:29 +0000

    finished

--- date.js
+++ date.js
@@ -1,5 +1,14 @@
 function getDate() 
 {
-	var dateString = new Date();
-	document.getElementById("date").innerHTML=dateString;
+  var dateString = new Date();
+  document.getElementById("date").innerHTML=dateString;
 }
+function cornsilk()
+{
+  document.getElementById("date").style.color="Green";
+}
+function firebrick()
+{
+  document.getElementById("date").style.color="firebrick";
+}
+
